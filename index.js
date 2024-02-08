e66d35a4a5489f468679f06b2a8b3f60af351de9
Ian Marshall <ian.js.tutor@gmail.com>
2024-02-08 11:41:30 -0500

    live branch

--- index.js
+++ index.js
@@ -1,29 +1,39 @@
 //dependencies
-import { fullscreenCanvas } from "./fullscreenCanvas.js";
 import { update as particlesUpdate, draw as particlesDraw, setEmitter as particlesSetEmitter } from "./particles.js";
 import { update as smokeUpdate, draw as smokeDraw, setEmitter as smokeSetEmitter } from "./smoke.js";
 import { update as sparklesUpdate, draw as sparklesDraw, setEmitter as sparklesSetEmitter } from "./sparkles.js";
 
 //environment
-const canvas = document.querySelector("canvas");
-const ctx = fullscreenCanvas(canvas, window);
+const width = 300;
+const height = 300;
+const canvases = [...document.querySelectorAll("canvas")];
+const contexts = [];
+canvases.forEach(c => {
+    c.width = width;
+    c.height = height;
+    contexts.push(c.getContext("2d"));
+});
 
 //loop
 function loop(t) {
     //erase
-    const { width, height } = canvas;
-    ctx.clearRect(0, 0, width, height);
+    contexts.forEach(ctx => ctx.clearRect(0, 0, width, height));
     //particles
-    update(canvas);
-    draw(ctx);
+    particlesUpdate(canvases[0]);
+    particlesDraw(contexts[0]);
+    smokeUpdate(canvases[1]);
+    smokeDraw(contexts[1]);
+    sparklesUpdate(canvases[2]);
+    sparklesDraw(contexts[2]);
     //repeat
     requestAnimationFrame(loop);
 }
 
 //init
 function init() {
-    canvas.addEventListener("click", setEmitter);
-    update(canvas);
+    particlesSetEmitter({ x: 150, y: 150 });
+    smokeSetEmitter({ x: 150, y: 150 });
+    sparklesSetEmitter({ x: 150, y: 150 });
     requestAnimationFrame(loop);
 }
 
